0f7f49df2f4c55b7129dda89f182fda6abd0ffc5
James Petullo <pentester18@gmail.com>
2018-02-15 22:33:44 -0500

    Update mylang_errors.py

--- mylang/mylang_errors.py
+++ mylang/mylang_errors.py
@@ -1,8 +1,25 @@
 class IllegialPrecedence(Exception):
-    pass
+    def __init__(self, message):
+        Exception.__init__(self, message)
 
 class VariableNotDeclared(Exception):
     pass
 
 class IncompatableTypes(Exception):
     pass
+
+class TooManyParamemters(Exception):
+    pass
+
+class ParameterSytnaxError(Exception):
+    pass
+
+class ReachedEndOfScopeBlock(Exception):
+    pass
+
+class InvalidScopeBlock(Exception):
+    pass
+
+class AttributeNotFound(Exception):
+    pass
+
