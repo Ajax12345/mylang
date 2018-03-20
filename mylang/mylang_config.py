385da7ffbf231c1649a1c3e5c45110d241d53fc9
James Petullo <pentester18@gmail.com>
2018-03-19 20:43:16 -0400

    Update mylang_config.py

--- mylang/mylang_config.py
+++ mylang/mylang_config.py
@@ -1,5 +1,6 @@
 import collections
 import abc
+__all__ = ['Config', 'config', 'ObjectifyImport', 'ImportAll', 'options']
 class Options:
     __metaclass__ = abc.ABCMeta
     @abc.abstractmethod
@@ -15,5 +16,24 @@ class Config(Options):
     def parse_assign(self):
         return
 
-options = collections.namedtuple('options', ['MAX_PARAMS'])
-config = options(5)
+class ImportAll:
+    def __init__(self):
+        self.import_flag = True
+        self.message = 'import all'
+        self.rep = '*'
+    def __repr__(self):
+        return str(self)
+    def __str__(self):
+        return 'ImportObject(message="{message}")'.format(**self.__dict__)
+
+class ObjectifyImport:
+    def __init__(self):
+        self.import_flag = False
+        self.message = 'keep as object'
+    def __repr__(self):
+        return str(self)
+    def __str__(self):
+        return 'ImportObject(message="{message}")'.format(**self.__dict__)
+
+options = collections.namedtuple('options', ['MAX_PARAMS', 'MAX_SCOPE_PARAMS', 'ALLOWED_RETURN_TYPES', 'PRIVATE_VARIABLE_LEVEL', 'PRIVATE_PROCEDURE_LEVEL', 'MAX_PROCEDURE_PARAMS'])
+config = options(5, 5, ['int', 'string', 'str', 'bool', "ArrayList"], True, False, 10)
